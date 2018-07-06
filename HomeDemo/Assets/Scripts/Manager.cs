4f145bacbcc39c9eb40c8e6f5bd11ad1bab289a7
jasontung2214 <jasontung2214@gmail.com>
2018-07-06 21:29:04 +0800

    新增家具 修改index

---
+++ HomeDemo/Assets/Scripts/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+    public GameObject housePlan;
+	private void OnGUI()
+    {
+        if(GUILayout.Button("開關平面圖"))
+        {
+            housePlan.SetActive(!housePlan.activeInHierarchy);
+        }
+    }
+}
